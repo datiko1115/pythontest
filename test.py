2756a7675f6d04d59a33f4dd4c0007e22222ba2d
datiko1115 <datiko1115@gmail.com>
2023-04-15 22:57:20 +0900

    演算

--- test.py
+++ test.py
@@ -11,3 +11,25 @@ print(1+1)
 print(1-1)
 print(10/2)
 print(10*2)
+
+#変数
+size = "l_size"
+num1 = 5
+num2 = 5.5
+
+print(size)
+print(type(size))
+
+print(num1)
+print(type(num1))
+
+print(num2)
+print(type(num2))
+
+print(num1 + num2)
+
+permit  = True #False
+
+
+
+
